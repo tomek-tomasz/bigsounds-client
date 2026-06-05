7db46d854ba5f4c91158b48d8cfb5fa63c726cd5
PomidorKing <jaroslaw.makucha@gmail.com>
2026-06-05 22:47:50 +0200

    refactor and minor fixes

--- src/main/java/com/example/bigsoundsclient/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.bigsoundsclient;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(BigSoundsApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,17 @@ module com.example.bigsoundsclient {
     requires java.desktop;
     requires com.google.gson;
 
-    opens com.example.bigsoundsclient to javafx.fxml;
-    exports com.example.bigsoundsclient;
+    opens com.example.bigsoundsclient.app      to javafx.fxml;
+    opens com.example.bigsoundsclient.api      to javafx.fxml;
+    opens com.example.bigsoundsclient.ui.base  to javafx.fxml;
+    opens com.example.bigsoundsclient.ui.auth  to javafx.fxml;
+    opens com.example.bigsoundsclient.ui.main  to javafx.fxml;
+    opens com.example.bigsoundsclient.ui.tabs  to javafx.fxml;
+
+    exports com.example.bigsoundsclient.app;
+    exports com.example.bigsoundsclient.api;
+    exports com.example.bigsoundsclient.ui.base;
+    exports com.example.bigsoundsclient.ui.auth;
+    exports com.example.bigsoundsclient.ui.main;
+    exports com.example.bigsoundsclient.ui.tabs;
 }
